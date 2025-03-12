d563f83d811a32e06bf5bfd56bf809a85c189b12
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2025-03-12 21:24:55 +0530

    doucments

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -16,15 +16,20 @@ const Sidebar = () => {
     const menuOptions = (
         <>
             <NewDocumentButton />
+
+            {/* MY Documents */}
+            {/* List */}
+            {/* Shared with me */}
+            {/* List ... */}
         </>
     );
 
   return (
-    <div className="p-2 md:p-5 bg-gray-200 relative">
-<div className="md:hidden bg-white" >
+    <div className="p-2 md:p-5 relative">
+<div className="md:hidden bg-white " >
 <Sheet >
     <SheetTrigger>
-        <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40}/>
+        <MenuIcon className=" p-2 hover:opacity-30 rounded-lg" size={40}/>
     </SheetTrigger>
     <SheetContent side="left">
         <SheetHeader>
