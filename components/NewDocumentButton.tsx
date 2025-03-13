3db012e5dd0d214a4a71eb56f37cfa6b18eb529d
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2025-03-13 23:22:58 +0530

    new document

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,8 +1,24 @@
+'use client';
+
+import { useTransition } from "react";
 import { Button } from "./ui/button"
+import { useRouter } from "next/navigation";
 
 const NewDocumentButton = () => {
+
+  const [isPending , startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleCreateNewDocument =  () => {
+    startTransition(async ()=> {
+      const {docId} = await createNewDocument();
+      router.push(`/doc/${docId}`);
+    })
+    
+  }
+
   return (
-    <Button>New Document</Button>
+    <Button onClick={handleCreateNewDocument}>New Document</Button>
   )
 }
 
